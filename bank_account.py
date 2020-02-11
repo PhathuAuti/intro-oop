8519d324834ce34ee26e646430322d5d25d40c6c
PhathuAuti <phathuauti@gmail.com>
2020-02-11 16:02:14 +0200

    Working On Inheritance

--- bank_account.py
+++ bank_account.py
@@ -16,9 +16,25 @@ class BankAccount:
     def withdraw(self, minus_amnt):
         self.balance = self.balance - minus_amnt
 
-bank_acc = BankAccount(1000, 0.12, 50)
+class Bank(BankAccount):
+    pass
 
-bank_acc.deposit(100)
-bank_acc.withdraw(300)
+    def __init__(self, balance, int_rate, month_fee, bank_account_number):
+        super().__init__(balance, int_rate, month_fee)
+        self.bank_account_number = bank_account_number
 
-print(bank_acc.finish_month())
+#     def withdraw(bank_account_number,amount):
+
+#     def deposit(bank_account_number,amount):
+
+#     def transfer(from_bank_account_number,to_bank_account_number, amount):
+
+bank_acc = Bank(1000, 0.12, 50, 1234567789)
+
+# bank_acc.deposit(100)
+# bank_acc.withdraw(300)
+
+# print(bank_acc.finish_month())
+
+print(bank_acc.balance)
+print(bank_acc.bank_account_number)
